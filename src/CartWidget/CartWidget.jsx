65e40560e4fe93953797ede5bf30cad03835b0a7
Dante pendino <dante.pendino25@gmail.com>
2024-02-23 15:56:06 -0300

    carrito de compra

--- src/CartWidget/CartWidget.jsx
+++ src/CartWidget/CartWidget.jsx
@@ -2,7 +2,7 @@ import cart from './assets/cart.svg'
 const CartWidget =() => {
     return (
         <div>
-            <img src={cart} alt= "cart-widget"/>
+            <img src={cart} alt= "cart-widget" height={15} width={15}/>
             0
         </div>
     )
